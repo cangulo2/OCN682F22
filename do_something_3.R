30c576dc292cfb873f0765393a1993ad46c4af17
JayQuelin <jacqueline7h@gmail.com>
2022-09-10 15:43:28 -1000

    Just have to add in diagram and answer for q 7

---
+++ do_something_3.R
@@ -0,0 +1,8 @@
+do_something_3 <- function(any_vec){
+  some_result_3 <- c[1:3]
+  some_result_3[1]<- sum(any_vec)
+  some_result_3[2]<- any_vec[1]
+  some_result_3[3]<- any_vec[1]/5
+  return (some_result_3)
+}
+any_vec
